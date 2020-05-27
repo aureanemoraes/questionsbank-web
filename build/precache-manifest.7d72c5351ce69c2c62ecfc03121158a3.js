c4605521908347c935531ecab01e95d078d66035
Aureane Moraes <aureane.moraes@outlook.com>
2020-05-27 13:47:49 -0300

    remove favcon

---
+++ build/precache-manifest.7d72c5351ce69c2c62ecfc03121158a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cb39cd6ce79d5ff45734a6b023c90c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18914d89ec1986c8911f",
+    "url": "/static/js/2.5f52540d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.5f52540d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6efe645345c69237e8a0",
+    "url": "/static/js/main.e7c33e36.chunk.js"
+  },
+  {
+    "revision": "1d89864f571e022585ef",
+    "url": "/static/js/runtime-main.229b3bb6.js"
+  },
+  {
+    "revision": "f504c4bfba84a0c63a1d858495782c4c",
+    "url": "/static/media/logo-seed.f504c4bf.png"
+  }
+]);
